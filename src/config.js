b21ca74342aeb3f6b2f245218950fbbaa0c18c43
Nikko Ortega <NFCOrtega89@gmail.com>
2017-08-01 19:01:17 -0700

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const API_URL = process.env.API_URL ||
-                'http://localhost:8080'
-
+const API_URL = process.env.NODE_ENV === 'production'
+                ? 'https://google.com'
+                : 'https://frozen-waters-55431.herokuapp.com/'
+console.log('NODE_ENV', process.env.NODE_ENV)
 module.exports = { API_URL }
