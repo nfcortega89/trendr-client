4e6e440e43104f36ce07fc3ebb0255b448d546ea
Niccolo Ortega <nfcortega89@gmail.com>
2018-01-21 14:33:09 -0800

    added overflow hidden to sliders

--- src/containers/CategoryPage.js
+++ src/containers/CategoryPage.js
@@ -1,57 +1,62 @@
-import React, { Component } from 'react'
-import Category from '../components/Category'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import * as actions from '../actions/categoryActions'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import Category from "../components/Category";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../actions/categoryActions";
+import { Link } from "react-router-dom";
 
 class CategoryPage extends Component {
   constructor(props) {
-    super(props)
-    this.renderCategories = this.renderCategories.bind(this)
+    super(props);
+    this.renderCategories = this.renderCategories.bind(this);
   }
   componentDidMount() {
-    this.props.actions.categoryDataRequest()
+    this.props.actions.categoryDataRequest();
   }
   renderCategories() {
     return this.props.categories.map(category => {
       return (
-        <Link className="category-wrapper" key={category._id} to={`/gallery/${category.title}`}>
-          <Category category={category} featuredurl={''} />
+        <Link
+          className="category-wrapper"
+          key={category._id}
+          to={`/gallery/${category.title}`}>
+          <Category category={category} featuredurl={""} />
         </Link>
-      )
-    })
+      );
+    });
   }
   render() {
+    if (!this.props.categories.length) {
+      return (
+        <div className="loader-wrapper">
+          <div className="loader" />;
+        </div>
+      );
+    }
     return (
       <div className="category-container">
-        <div className="category-list">
-          {this.renderCategories()}
-        </div>
+        <div className="category-list">{this.renderCategories()}</div>
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps(state) {
   return {
     categories: state.category.categories
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(actions, dispatch)
-  }
+  };
 }
 
 CategoryPage.propTypes = {
   categories: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
-}
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CategoryPage)
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryPage);
